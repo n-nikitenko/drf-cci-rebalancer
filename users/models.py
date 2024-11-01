0c01100c338d30a4ef15002a10cdfe814d7d4b0c
nnikitenko <nnikitenko@aorti.ru>
2024-11-01 16:41:36 +0300

    добавлены необходимые библиотеки, создано приложение для управления пользователями

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """модель для пользователя"""
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
